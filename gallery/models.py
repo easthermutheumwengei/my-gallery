44e74727da440cbc0f61ac4d1373a243a526ea8d
easthermutheumwengei <easther.mwengei@student.moringaschool.com>
2021-09-05 17:12:34 +0300

    create test for location

--- gallery/models.py
+++ gallery/models.py
@@ -1,7 +1,9 @@
+from django.core import exceptions
 from django.db import models
+import datetime as dt
+
 
 # Create your models here.
-
 class Location(models.Model):
     location_name = models.CharField(max_length=30)
 
@@ -17,10 +19,10 @@ class Location(models.Model):
     @classmethod
     def display_all_image_locations(cls):
         return cls.objects.all()
-    @classmethod
-    def update_location(cls,id,location_name):
-        cls.objects.filter(id = id).update(location_name = location_name)
 
+    @classmethod
+    def update_location(cls, id, location_name):
+        cls.objects.filter(id=id).update(location_name=location_name)
 
 
 class Categories(models.Model):
@@ -40,12 +42,13 @@ class Categories(models.Model):
         return cls.objects.all()
 
     @classmethod
-    def update_category(cls,id,category):
-        cls.objects.filter(id = id).update(category = category)
+    def update_category(cls, id, category):
+        cls.objects.filter(id=id).update(category=category)
+
 
 class Image(models.Model):
-    image= models.ImageField(upload_to ='gallery')
-    image_name = models.CharField(max_length =60)
+    image = models.ImageField(upload_to='gallery')
+    image_name = models.CharField(max_length=60)
     image_description = models.CharField(max_length=250)
     image_location = models.ForeignKey(Location, on_delete=models.CASCADE)
     image_category = models.ForeignKey(Categories, on_delete=models.CASCADE)
@@ -93,3 +96,5 @@ class Image(models.Model):
 
     class Meta:
         ordering = ['post_date']
+
+
--- gallery/tests.py
+++ gallery/tests.py
@@ -1,3 +1,24 @@
 from django.test import TestCase
-
+from .models import Location,Categories,Image
+import datetime as dt
 # Create your tests here.
+class LocationTestClass(TestCase):
+    def setUp(self):
+        self.Westlands = Location(location_name='Nairobi')
+
+    def test_instance(self):
+        self.assertTrue(isinstance(self.Nairobi,Location))
+
+    def tearDown(self):
+        Location.objects.all().delete()
+
+    def test_save_method(self):
+        self.Nairobi.save_location()
+        locations = Location.objects.all()
+        self.assertTrue(len(locations)>0)
+
+    # def test_delete_method(self):
+    #     self.Nairobi.delete_location()
+    #     locations = Location.objects.all()
+    #     self.assertTrue(len(locations)==0)
+
