ab0f43b0e2e548b0aec4c5d57d353bf6a24e0e7e
easthermutheumwengei <easther.mwengei@student.moringaschool.com>
2021-09-05 16:34:13 +0300

    create location methods

--- gallery/models.py
+++ gallery/models.py
@@ -8,6 +8,21 @@ class Location(models.Model):
     def __str__(self):
         return self.location_name
 
+    def save_location(self):
+        self.save()
+
+    def delete_location(self):
+        self.delete()
+
+    @classmethod
+    def display_all_image_locations(cls):
+        return cls.objects.all()
+    @classmethod
+    def update_location(cls,id,location_name):
+        cls.objects.filter(id = id).update(location_name = location_name)
+
+
+
 class Categories(models.Model):
     category = models.CharField(max_length=30)
 
