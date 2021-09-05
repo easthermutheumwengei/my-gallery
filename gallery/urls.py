25ca0eb6fc6d4a118c856d01917267e0e7d539c6
easthermutheumwengei <easther.mwengei@student.moringaschool.com>
2021-09-05 17:43:31 +0300

    update urls

--- gallery/urls.py
+++ gallery/urls.py
@@ -1,7 +1,14 @@
 from django.conf.urls import url
 from . import views
-
+from django.conf import settings
+from django.conf.urls.static import static
 urlpatterns=[
     url('^$',views.home,name = 'home'),
-]
+    url(r'location/(?P<location_name>\w+)/', views.image_location, name='location'),
+    url(r'category/(?P<category>\w+)/', views.image_category, name='category'),
+    url(r'^search/', views.search_results, name='search_results'),
+    url('gallery', galleryView.as_view(), name = 'gallery'),
 
+]
+if settings.DEBUG:
+    urlpatterns+= static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
