b6b51b39049c423d95cd75700c0b85a84578bde9
easthermutheumwengei <easther.mwengei@student.moringaschool.com>
2021-09-05 17:34:26 +0300

    update views

--- gallery/views.py
+++ gallery/views.py
@@ -27,3 +27,52 @@ def gallery(request):
     }
 
     return render(request, 'gallery/gallery.html',context)
+
+class LocationView(ListView):
+    model= Location
+    template_name= 'gallery/gallery.html'
+
+class galleryView(ListView):
+        model = Image
+        template_name = 'gallery/gallery.html'
+
+class detailsView(DetailView):
+            model = Image
+            template_name = 'gallery/details.html'
+
+def image_location(request, location_name):
+                images = Image.filter_by_location(location_name)
+                # print(images)
+                context = {
+                    "title": location_name,
+                    "header": location_name,
+                    'location_images': images
+
+                }
+                return render(request, 'gallery/location.html', context)
+
+def image_category(request, category):
+                images = Image.filter_by_category(category)
+                # print(images)
+
+                context = {
+                    "title": category,
+                    "header": category,
+                    'category_images': images
+
+                }
+                return render(request, 'gallery/category.html', context)
+
+def search_results(request):
+    if 'category' in request.GET and request.GET["category"]:
+        search_category = request.GET.get("category")
+
+        searched_category_images = Image.search_by_category(search_category)
+
+        message = f"{search_category}"
+
+        return render(request, 'gallery/search.html',{"message":message,"category_images": searched_category_images})
+
+    else:
+        message = "No such word"
+        return render(request, 'gallery/search.html',{"message":message})
